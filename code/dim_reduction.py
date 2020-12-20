8a24ca066e608d190874d17746c44bd0fb834594
MikeLasz <mike.laszkiewicz@rub.de>
2020-12-20 15:08:34 +0100

    added pca and tsne

---
+++ code/dim_reduction.py
@@ -0,0 +1,30 @@
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+
+# get the embeddings
+data = "aifb" # aifb, mutag, or bgs
+nlp_model = "cbow" # skip-gram or cbow
+num_embeddings = 10
+embeddings = []
+for iter in range(num_embeddings):
+    embeddings.append(np.load("embeddings/" + data + "/" + nlp_model + "/embedding" + str(iter) + ".npy"))
+
+# apply dimension reduction
+dim_red = "tsne2d" # "pca2d", "pca3d", "tsne2d", "tsne3d"
+reduced_embeddings = []
+for iter in range(num_embeddings):
+    if dim_red=="pca2d":
+        pca = PCA(n_components=2)
+        transformed_embedding = pca.fit_transform(embeddings[iter])
+    elif dim_red=="pca3d":
+        pca = PCA(n_components=3)
+        transformed_embedding = pca.fit_transform(embeddings[iter])
+    elif dim_red=="tsne2d":
+        tsne = TSNE(n_components=2, random_state=42)
+        transformed_embedding = tsne.fit_transform(embeddings[iter])
+    reduced_embeddings.append(transformed_embedding)
+
+# save the embeddings
+for iter in range(num_embeddings):
+    np.save("embeddings/" + data + "/" + nlp_model + "/" + dim_red + "/embedding" + str(iter), reduced_embeddings[iter])
--- code/stability_analysis.py
+++ code/stability_analysis.py
@@ -1,19 +1,30 @@
 import numpy as np
 from jaccard_similarity import jaccard_emb, jaccard_of_sets
 
-data = "mutag" # aifb, mutag, or bgs
-nlp_model = "skip-gram" # skip-gram or cbow
+data = "aifb" # aifb, mutag, or bgs
+nlp_model = "cbow" # skip-gram or cbow
 num_embeddings = 10
 embeddings = []
-for iter in range(num_embeddings):
-    embeddings.append(np.load("embeddings/" + data + "/" + nlp_model + "/embedding" + str(iter) + ".npy"))
 
+dim_red = "tsne2d" # empty string "", "pca2d", "pca3d", "tsne2d", or "tsne3d"
+
+# read embeddings:
+if dim_red=="":
+    for iter in range(num_embeddings):
+        embeddings.append(np.load("embeddings/" + data + "/" + nlp_model + "/embedding" + str(iter) + ".npy"))
+else:
+    for iter in range(num_embeddings):
+        embeddings.append(np.load("embeddings/" + data + "/" + nlp_model + "/" + dim_red + "/embedding" + str(iter) + ".npy"))
+
+# compute the score:
 score = np.zeros(num_embeddings)
 print("Results " + data)
-for j in range(1,num_embeddings):
+for j in range(1, num_embeddings):
     score_iter = jaccard_emb(embeddings[0:j])
     score[j] = np.mean(score_iter)
     print("mean (iter:" + str(j) + ") = " + str(score[j]))
     print("var = " + str(np.var(score_iter)))
 
-np.save("scores/" + data + "/" + nlp_model + "_score.npy", score)
+
+# save the results:
+np.save("scores/" + data + "/" + nlp_model + dim_red + "_score.npy", score)
