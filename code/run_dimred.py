b05dfb5f31e9519dea47de81c0ba4fb7a85dc6fd
MikeLasz <mike.laszkiewicz@rub.de>
2021-03-22 18:01:07 +0100

    python -> python3

--- code/run_dimred.py
+++ code/run_dimred.py
@@ -2,11 +2,11 @@ import subprocess as s
 
 test = False
 if test:
-    s.run(["python", "dim_reduction.py", "--data", "aifb"])
+    s.run(["python3", "dim_reduction.py", "--data", "aifb"])
 else:
     #for data in ("aifb", "bgs", "mutag"):
     for data in ("bgs", "mutag"):
         for nlp_model in ("cbow", "skip-gram"):
             for dim_red in ("pca2d", "pca3d", "tsne2d", "tsne3d"):
                 print("Running: " + data + ", " + nlp_model + ", " + dim_red)
-                s.run(["python", "dim_reduction.py", "--data", data, "--nlp_model", nlp_model, "--dim_red", dim_red])
+                s.run(["python3", "dim_reduction.py", "--data", data, "--nlp_model", nlp_model, "--dim_red", dim_red])
--- code/run_stab_ana.py
+++ code/run_stab_ana.py
@@ -2,19 +2,19 @@ import subprocess as s
 
 test = True
 if test:
-    s.run(["python", "stability_analysis.py", "--data", "aifb", "--criterion", "cosine-knn", "--dim_red", "no" ])
+    s.run(["python3", "stability_analysis.py", "--data", "aifb", "--criterion", "cosine-knn", "--dim_red", "no" ])
 else:
     for data in ("aifb", "bgs", "mutag"):
         for nlp_model in ("cbow", "skip-gram"):
             for dim_red in ("pca2d", "pca3d", "tsne2d", "tsne3d"):
                 # Run euclidean k-NN only for reduced data
                 print("Running: " + data + ", " + nlp_model + ", " + dim_red)
-                s.run(["python", "stability_analysis.py", "--data", data, "--nlp_model", nlp_model, "--dim_red", dim_red,
+                s.run(["python3", "stability_analysis.py", "--data", data, "--nlp_model", nlp_model, "--dim_red", dim_red,
                        "--criterion", "euclidean-knn"])
     for data in ("aifb", "bgs", "mutag"):
         for nlp_model in ("cbow", "skip-gram"):
             for criterion in ("cosine-knn", "aligned_cosine", "sec_order_cosine"):
                 # The other criteria do not require to employ a dimension reduction beforehand.
                 print("Running: " + data + ", " + nlp_model + ", " + criterion)
-                s.run(["python", "stability_analysis.py", "--data", data, "--nlp_model", nlp_model, "--dim_red", "no",
+                s.run(["python3", "stability_analysis.py", "--data", data, "--nlp_model", nlp_model, "--dim_red", "no",
                        "--criterion", criterion])
