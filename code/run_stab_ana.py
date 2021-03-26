edb1a11fadffd16cbac3b0e22d9213284f1f621c
Ubuntu <ubuntu@ip-172-31-24-94.us-east-2.compute.internal>
2021-03-26 09:33:04 +0000

    run simulations on aws(aifb and bgs)

--- code/run_stab_ana.py
+++ code/run_stab_ana.py
@@ -1,6 +1,6 @@
 import subprocess as s
 
-test = True
+test = False
 if test:
     s.run(["python3", "stability_analysis.py", "--data", "aifb", "--criterion", "cosine-knn", "--dim_red", "no" ])
 else:
@@ -12,9 +12,10 @@ else:
                 s.run(["python3", "stability_analysis.py", "--data", data, "--nlp_model", nlp_model, "--dim_red", dim_red,
                        "--criterion", "euclidean-knn"])
     for data in ("aifb", "bgs", "mutag"):
-        for nlp_model in ("cbow", "skip-gram"):
-            for criterion in ("cosine-knn", "aligned_cosine", "sec_order_cosine"):
-                # The other criteria do not require to employ a dimension reduction beforehand.
-                print("Running: " + data + ", " + nlp_model + ", " + criterion)
-                s.run(["python3", "stability_analysis.py", "--data", data, "--nlp_model", nlp_model, "--dim_red", "no",
+    #for data in ("bgs", "mutag"):
+    	for nlp_model in ("cbow", "skip-gram"):
+    		for criterion in ("cosine-knn", "aligned_cosine", "sec_order_cosine"):
+                	# The other criteria do not require to employ a dimension reduction beforehand.
+       			print("Running: " + data + ", " + nlp_model + ", " + criterion)
+        		s.run(["python3", "stability_analysis.py", "--data", data, "--nlp_model", nlp_model, "--dim_red", "no",
                        "--criterion", criterion])
