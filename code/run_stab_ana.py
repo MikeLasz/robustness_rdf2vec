de11fc02d2648e37ffa8f37b6657338273eefb7c
MikeLasz <mike.laszkiewicz@rub.de>
2021-04-08 11:39:33 +0200

    changed run_stab_ana: only do the experiments for mutag

--- code/run_stab_ana.py
+++ code/run_stab_ana.py
@@ -9,13 +9,13 @@ else:
             for dim_red in ("pca2d", "pca3d", "tsne2d", "tsne3d"):
                 # Run euclidean k-NN only for reduced data
                 print("Running: " + data + ", " + nlp_model + ", " + dim_red)
-                s.run(["python3", "stability_analysis.py", "--data", data, "--nlp_model", nlp_model, "--dim_red", dim_red,
-                       "--criterion", "euclidean-knn"])
-    for data in ("aifb", "bgs", "mutag"):
-    #for data in ("bgs", "mutag"):
-    	for nlp_model in ("cbow", "skip-gram"):
-    		for criterion in ("cosine-knn", "aligned_cosine", "sec_order_cosine"):
-                	# The other criteria do not require to employ a dimension reduction beforehand.
-       			print("Running: " + data + ", " + nlp_model + ", " + criterion)
-        		s.run(["python3", "stability_analysis.py", "--data", data, "--nlp_model", nlp_model, "--dim_red", "no",
-                       "--criterion", criterion])
+                #s.run(["python3", "stability_analysis.py", "--data", data, "--nlp_model", nlp_model, "--dim_red", dim_red,
+                #       "--criterion", "euclidean-knn"])
+    #for data in ("aifb", "bgs", "mutag"):
+    data = "mutag"
+    for nlp_model in ("cbow", "skip-gram"):
+        for criterion in ("cosine-knn", "aligned_cosine", "sec_order_cosine"):
+                # The other criteria do not require to employ a dimension reduction beforehand.
+            print("Running: " + data + ", " + nlp_model + ", " + criterion)
+            s.run(["python3", "stability_analysis.py", "--data", data, "--nlp_model", nlp_model, "--dim_red", "no",
+                    "--criterion", criterion])
